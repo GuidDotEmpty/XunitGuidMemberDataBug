4600cdb0aaed4956a7d1d115caa9a14073efa3d7
Rob <86649932+GuidDotEmpty@users.noreply.github.com>
2026-02-13 14:22:08 +0100

    init projects

---
+++ XunitV2VsV3/V2/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace V2
+{
+    public class UnitTest1
+    {
+        public static TheoryData<Guid> Data => new TheoryData<Guid>
+        {
+            Guid.NewGuid(),
+            Guid.NewGuid()
+        };
+
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Test1(Guid id)
+        {
+            Assert.True(true);
+        }
+
+    }
+}
